a04d47d69937ba5f7d110acd53be5d67e09c8990
Simbamon <sbhan9511@gmail.com>
2023-04-23 13:41:36 +0900

    Dockerfile, k8s yaml and app updated

--- app/main.py
+++ app/main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
+import os
 
 app = FastAPI()
 
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"Hello": f"From: {os.environ.get('HOSTNAME', 'DEFAULT_ENV')}"}
